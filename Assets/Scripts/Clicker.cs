8701eb0ce2bda785639947c7713f1144b8d5323d
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2020-12-24 09:25:01 -0500

    Add files via upload

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public event Action Click;
+    public event Action Hover;
+    //UnityEvent m_MyEvent;
+
+
+    public string hovering = "None";
+    public string selected = "None";
+    public float clickPosX = 0;
+    public float clickPosY = 0;
+    public string State = "None";
+
+    public int hoverLength = 180;
+    public int hoverTime = 0;
+
+
+    public GameObject Heart;
+    public GameObject Armor;
+    public GameObject MagicArmor;
+
+    void Start() {
+        
+    }
+    //{
+    //    Debug.Log("FACK");
+        //if (m_MyEvent == null)
+        //    m_MyEvent = new UnityEvent();
+
+        //m_MyEvent.AddListener(Ping);
+    //}
+    // Update is called once per frame
+    void Update () {
+        
+        if (hovering != "None") {hovering = "None";}
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        
+
+
+        if (hit.collider != null) {
+            
+            hovering = hit.collider.gameObject.name;
+
+            if (hoverTime >= hoverLength) {
+                //Debug.Log("FACK");
+                Hover();
+            } else {
+                hoverTime++;
+            }
+
+
+            if (Input.GetMouseButtonDown(0)) {
+                if (selected == "None") {selected = hovering;} else {selected = "None";}
+                
+                clickPosX = mousePos.x;
+                clickPosY = mousePos.y;
+                Click();
+                //gameObject.SendMessage(, 5.0);
+            }
+
+            //Debug.Log(lastClicked);
+                //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            
+        } else { hoverTime = 0; } //if (hoverTime > 0) { hoverTime--; } else { hoverTime = 0;} }
+
+
+
+        if (Input.GetMouseButtonUp(0)) {
+
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            //RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            //if (hit.collider != null) {
+                clickPosX = 0;//mousePos.x;
+                clickPosY = 0;//mousePos.y;
+                
+                //Debug.Log(lastClicked);
+                //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            //}
+        }
+    }
+
+
+   
+
+    //For ease of access all types of damage moves are listed here. for now
+    /*
+    public int CLICKED(string name){
+        if (name == "Player")
+            return((int)(Heal*Mult*-1));
+        else
+            return((int)(Pain*Mult));
+    }
+    */
+}
---
+++ Assets/Scripts/Creatures.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creatures : MonoBehaviour
+{
+    private Clicker main;
+    private SpriteRenderer Body;
+    //private Creatures Player;
+
+
+    //globals for the creature
+    //public string name;
+
+    public int HP;
+    public int MaxHP;
+    public int Armor;
+    public int MagicArmor;
+    
+    public int MaxStrain;
+    public int Strain;
+    public int StrainRegen;
+
+    public int Speed;
+
+    public string[] Bonuses = new string[6]; // the 6 is an arbitrary number i may use a list instead later
+
+    public int[] Pos = {0,0}; // x,y loc on grid
+
+    bool hover = false;
+
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (gameObject.name != "Player")
+        //    Player = GameObject.Find("Player").GetComponent<Creatures>();
+        main = GameObject.Find("Main Camera").GetComponent<Clicker>();
+        main.Click += Damage;
+        main.Hover += Hover;
+
+        Body = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hover) {Hover();}
+        
+        /*
+        if (gameObject.name != "Player")
+            if (damageTimer == 0){
+                Player.HP -= 1;
+                damageTimer = damageTimerMax;
+            }
+            else   
+                damageTimer -=1;
+
+            
+
+
+        if (Timmer == 0){
+            Body.color = Color.white;
+            Timmer -= 1;
+        }else
+            Timmer -= 1;
+
+        if (main.clicked == gameObject.name){
+            Damage();
+        }
+        */
+    }
+
+    void Damage(){
+        if (main.selected == gameObject.name){
+            Debug.Log(gameObject.name);
+        }
+        /*
+        int damage = main.CLICKED(gameObject.name);
+        HP -= damage;
+
+        //main.State = gameObject.name;
+        if (damage > 0){
+            Body.color = Color.red;
+            Timmer = damage/2;
+        }else{
+            Body.color = Color.cyan;
+            Timmer = (damage*-1)/2;
+        }
+
+        if(HP <= 0){
+            HP = 0;
+            Destroy(gameObject);
+        }
+        if(HP > MaxHP)
+            HP = MaxHP;
+        */
+    }
+
+
+
+
+    void Hover() {
+        if (main.hovering == gameObject.name && !hover){
+            Body.color = Color.red;
+            HeathGui();
+            hover = true;
+        } else if (main.hovering != gameObject.name && hover) {
+            Body.color = Color.white;
+            hover = false;
+        }
+    }
+
+    
+    void HeathGui() {
+        int i = (int)(-0.4f * (HP + Armor + MagicArmor));
+        Debug.Log(i);
+        for (int a = 0; a < HP; a++){
+            GameObject bab = Instantiate(main.Heart, new Vector3(i * 0.9F, 1.3f, 0), Quaternion.identity);
+            bab.transform.parent = gameObject.transform;
+            i++;
+        }
+
+        //for (int a = 0; a < (MaxHP - HP); a++){
+        //    hpList += main.hpSprites[8];
+        //}
+
+        for (int a = 0; a < Armor; a++){
+            GameObject bab = Instantiate(main.Armor, new Vector3(i * 0.9F, 1.3f, 0), Quaternion.identity);
+            bab.transform.parent = gameObject.transform;
+            i++;
+        }
+
+        for (int a = 0; a < MagicArmor; a++){
+            GameObject bab = Instantiate(main.MagicArmor, new Vector3(i * 0.9F, 1.3f, 0), Quaternion.identity);
+            bab.transform.parent = gameObject.transform;
+            i++;
+        }
+
+
+
+    }
+    //*/
+}
